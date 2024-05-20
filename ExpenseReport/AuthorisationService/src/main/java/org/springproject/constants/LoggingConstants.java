c4cfab12a51dfaade2d841959f6372bd33a0f163
Tanvi <tanvi2808@gmail.com>
2024-05-20 22:26:48 +0530

    Delete ExpenseReport directory

--- ExpenseReport/AuthorisationService/src/main/java/org/springproject/constants/LoggingConstants.java
+++
@@ -1,7 +0,0 @@
-package org.springproject.constants;
-
-public class LoggingConstants {
-
-    public static final String START_INFO_CONSTANT = "Start : {}.{} : {}";
-    public static final String END_INFO_CONSTANT = "End : {}.{}";
-}
--- ExpenseReport/AuthorisationService/src/main/java/org/springproject/service/AuthService.java
+++
@@ -1,8 +0,0 @@
-package org.springproject.service;
-
-import org.springproject.model.SignUpRequest;
-
-public interface AuthService {
-
-    String signUp(SignUpRequest signUpRequest);
-}
